fabef65432eae898123264b28c1f13dab1666dab
ALEX ARANTES GONCALVES <26131401861@info.local>
2024-10-01 21:58:49 -0300

    Consolidacao SQL- Subqueries

--- 20240930_SQL_Continuacao/desafio.sql
+++ 20240930_SQL_Continuacao/desafio.sql
@@ -1,5 +1,7 @@
 CREATE DATABASE db_biblioteca;
 USE db_biblioteca;
+
+SET @@autocommit = ON;
 -- DDL
 CREATE TABLE tb_autores(
 	id_autor INT(10) NOT NULL
@@ -7,7 +9,6 @@ CREATE TABLE tb_autores(
     ,data_nascimento DATE NOT NULL
     ,PRIMARY KEY(id_autor)
 	);
-    
 CREATE TABLE tb_livros(
 	id_livro INT(10) NOT NULL
     ,titulo VARCHAR(100) NOT NULL
@@ -16,14 +17,12 @@ CREATE TABLE tb_livros(
     ,PRIMARY KEY(id_livro)
     ,FOREIGN KEY(id_autor) REFERENCES tb_autores(id_autor)
 );
-
 CREATE TABLE tb_membros(
 	id_membro INT(10) NOT NULL
     ,nome VARCHAR(100) NOT NULL
     ,data_adesao DATE NOT NULL
     ,PRIMARY KEY(id_membro)
 );
-
 CREATE TABLE tb_emprestimos(
 	id_emprestimo INT(10) NOT NULL
     ,id_membro INT(10) NOT NULL
@@ -36,21 +35,168 @@ CREATE TABLE tb_emprestimos(
 );
 
 -- DML EXERCICIO 3. Inserir ao menos 3 valores em cada tabela
+
 INSERT INTO tb_autores VALUES(
 	'1','Agatha Christe','1939-08-03'
 );
-
-);
 INSERT INTO tb_autores VALUES(
 	'3','Miguel Servantes','1914-07-28'
+);
+INSERT INTO tb_autores VALUES(
+	'2','Fedor Dostoyevsky','1839-03-11'
 );
 
 INSERT INTO tb_livros VALUES(
 	'1','O misterio dos 7 relogios','1929','1'
 );
-
 INSERT INTO tb_livros VALUES(
-	'2','O crime e o castigo','1'
-/*
+	'2','O crime e o castigo','1861','2'
+);
+INSERT INTO tb_livros VALUES(
+	'3','Dom Quixote','1919','3'
+);
 
-*/
+INSERT INTO tb_membros VALUES(
+	'10','Richardson Celeveu','2024-01-01'
+);
+INSERT INTO tb_membros VALUES(
+	'12','Andreleuza Gerevis','2024-01-02'
+);
+INSERT INTO tb_membros VALUES(
+	'14','Hesveralda Sincleir','2024-01-03'
+);
+
+INSERT INTO tb_emprestimos VALUES(
+	'100','10','1','2024-06-02','2024-06-17'
+);
+INSERT INTO tb_emprestimos VALUES(
+	'101','12','1','2024-07-01','2024-07-16'
+);
+INSERT INTO tb_emprestimos VALUES(
+	'102','14','3','2024-07-05','2024-07-20'
+);
+INSERT INTO tb_emprestimos VALUES(
+	'104','10','2','2024-07-08','2024-07-25'
+);
+
+-- a. O livro com a data de publicação mais antigo
+(SELECT
+	*
+FROM 
+	tb_livros
+WHERE
+	ano_publicacao = (SELECT MIN(ano_publicacao) FROM tb_livros)
+);
+    
+-- b. O livro com a data de publicação mais recente
+(SELECT
+	*
+FROM 
+	tb_livros
+WHERE
+	ano_publicacao = (SELECT MAX(ano_publicacao) FROM tb_livros)
+);
+-- c. A quantidade de livros cadastrados no banco de dados
+
+(SELECT
+	count(id_livro)
+FROM 
+	tb_livros
+);
+-- d. Consulte apenas os livros que possuam a data de devolução em 18-07-2024
+
+(SELECT
+	*
+FROM 
+	tb_emprestimos
+WHERE
+	dt_devolucao = '2024-06-17'
+);
+-- e. Ordene o nome dos autores em ordem crescente
+
+(SELECT
+	*
+FROM 
+	tb_autores
+ORDER BY nome ASC
+);
+
+-- f. Ordene o nome dos livros em ordem decrescente
+(SELECT
+	*
+FROM 
+	tb_livros
+ORDER BY titulo ASC
+);
+
+/*EXERCÍCIO 5. Utilize subqueries para realizar as seguintes consultas:*/
+-- a. Liste os autores que possuem livros com ano de publicação maior que a média de anos de publicao dos livros calculasos
+/*SELECT
+	aut.nome
+FROM
+	tb_autores aut
+    INNER JOIN tb_livros lvr
+			ON aut.id_autor = lvr.id_autor
+WHERE
+	ano_publicacao > (SELECT AVG(ano_publicacao) FROM tb_livros);
+ */
+(SELECT
+	nome
+ FROM 
+	tb_autores
+ WHERE
+	id_autor IN (SELECT 
+					id_autor
+				 FROM 
+					 tb_livros 
+				 WHERE 
+                     ano_publicacao > (SELECT 
+										  AVG(ano_publicacao) 
+									   FROM 
+										   tb_livros
+									)
+				 )
+);
+
+-- b. Encontre os membros que realizaram mais de um empréstimo.
+(SELECT
+	nome
+FROM
+	tb_membros
+WHERE 
+	id_membro IN (SELECT
+					  id_membro
+				  FROM
+					  tb_emprestimos
+				  GROUP BY
+					  id_membro 
+				  HAVING
+                      COUNT(id_membro) >1
+				  )
+);
+
+-- c. Liste os livros que foram emprestados ao menos uma vez.    
+(SELECT
+	titulo
+FROM
+	tb_livros
+WHERE 
+	id_livro IN (SELECT
+					  id_livro
+				  FROM
+					  tb_emprestimos
+				  )
+);
+
+-- d. Consulte os livros que ainda não foram emprestados.
+(SELECT
+	titulo
+FROM
+	tb_livros
+WHERE 
+	id_livro NOT IN (SELECT
+					  id_livro
+				  FROM
+					  tb_emprestimos
+				  )
+);
